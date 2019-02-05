c3a2dd13fcb1cfa6d24b7215b4d622ac6eb6dc80
Preprocessing - I <karenly1961@gmail.com>
2019-02-05 18:40:40 -0500

    Added Content Text

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-class Subject {
-    constructor(subject) {
 
-      this.subject = subject;
-      
-      this.subjectButtons.addEventListener('click', () => this.toggleSubject());
-      
-    }
-    toggleSubject() {
-     
-    }
-  }
   
   const subjects = document.querySelectorAll('.subject');
   subjects.forEach( subject => new Subject(subject));
