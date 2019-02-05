27d8b18763cad436e26d6a9ace7d36c5ccc8b86d
Preprocessing - I <karenly1961@gmail.com>
2019-02-05 16:57:44 -0500

    Link to REACT Page

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ class Subject {
     constructor(subject) {
 
       this.subject = subject;
-      this.subjectButtons = subject.querySelector('.subject-button');
-      this.subjectBtnOpen = subject.querySelector('.subject-btn-open');
-      this.subjectBtnClose = subject.querySelector('.subject-btn-close');
-      this.subjectContent = subject.querySelector('.subject-content');
+      
       this.subjectButtons.addEventListener('click', () => this.toggleSubject());
       
     }
     toggleSubject() {
-      this.subjectBtnOpen.classList.toggle('hide-btn');
-      this.subjectBtnClose.classList.toggle('hide-btn');
-      this.subjectContent.classList.toggle('toggle-on');
+     
     }
   }
   
